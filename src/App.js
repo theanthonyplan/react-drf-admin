fe495e1347d5f1445feddd91edbdef12f799438f
avg <p0rp@p0rp.com>
2017-01-07 18:25:46 -0500

    implemented list, create,edit, delete, and relationships for the jsonApiRestClient

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-
-import { PostList } from './posts';
+import { Delete } from 'admin-on-rest/lib/mui';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} />
         <Resource name="users" list={UserList} />
     </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,13 +1,47 @@
 // in src/posts.js
 import React from 'react';
-import { List, Datagrid, TextField } from 'admin-on-rest/lib/mui';
+import { List, Edit, Create, Datagrid, ReferenceField, TextField, EditButton, DisabledInput, LongTextInput, ReferenceInput, SelectInput, TextInput } from 'admin-on-rest/lib/mui';
 
 export const PostList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="id" />
+              <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+              </ReferenceField>
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+
+
+export const PostEdit = (props) => (
+    <Edit title={<PostTitle />} {...props}>
+        <DisabledInput source="id" />
+        <ReferenceInput label="User" source="userId" reference="users">
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title" />
+        <LongTextInput source="body" />
+    </Edit>
+);
+
+
+
+
+export const PostCreate = (props) => (
+    <Create {...props}>
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title" />
+        <LongTextInput source="body" />
+    </Create>
 );
