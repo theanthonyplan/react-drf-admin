4d0cec413f404b5314b3e4d49482516cbb9b5c91
avg <p0rp@p0rp.com>
2017-01-07 18:12:23 -0500

    added users resource

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
 import { PostList } from './posts';
+import { UserList } from './users';
 
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
         <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
     </Admin>
 );
 
---
+++ src/posts.js
@@ -0,0 +1,13 @@
+// in src/posts.js
+import React from 'react';
+import { List, Datagrid, TextField } from 'admin-on-rest/lib/mui';
+
+export const PostList = (props) => (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="id" />
+            <TextField source="title" />
+            <TextField source="body" />
+        </Datagrid>
+    </List>
+);
