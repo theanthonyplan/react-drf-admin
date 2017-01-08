e56b163c12a176c2d9670d104f6b1a62c1ff5cb1
avg <p0rp@p0rp.com>
2017-01-07 20:45:22 -0500

    moved backends into project.  added custom restClient to admin front end.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // in src/App.js
 import React from 'react';
+import {jsonServerRestClient} from 'admin-on-rest';
+import { Admin, Resource } from 'admin-on-rest';
+import myApiRestClient from './restClient';
 
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { Delete } from 'admin-on-rest/lib/mui';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
@@ -12,10 +14,16 @@ import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
 
 const App = () => (
-    <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}  icon={PostIcon} />
-        <Resource name="users" list={UserList} icon={UserIcon} />
-    </Admin>
+  <div>
+      <Admin dashboard={Dashboard} restClient={myApiRestClient}>
+          <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}  icon={PostIcon} />
+          <Resource name="users" list={UserList} icon={UserIcon} />
+      </Admin>
+      <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+          <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}  icon={PostIcon} />
+          <Resource name="users" list={UserList} icon={UserIcon} />
+      </Admin>
+  </div>
 );
 
 export default App;
